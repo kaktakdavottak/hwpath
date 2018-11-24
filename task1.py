88126d6761219e2d0648159cc0b27f14e2c4a777
Mineev Semen <semenmineev@gmail.com>
2018-11-24 23:43:34 +0300

    Исправлена

--- task1.py
+++ task1.py
@@ -1,25 +1,29 @@
 import os
-import re
 
 
 def get_files_list():
     migrations = 'Migrations'
+    file_format = '.sql'
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    list_file = os.listdir(path=os.path.join(current_dir, 'migrations'))
+    list_file = []
+    for sql_file in os.listdir(path=os.path.join(current_dir, 'migrations')):
+        if file_format in sql_file:
+            list_file.append(sql_file)
     return list_file
 
 
 def find_procedure():
     list_file = get_files_list()
     while True:
-        user_input = input('Введите строку:').lower()
+        user_input = input('Введите строку:')
         file_list_after_search = []
         i = 0
         for files in list_file:
-            if re.search(user_input, files.lower()) and re.search('.sql', files):
-                print(files)
-                i += 1
-                file_list_after_search.append(files)
+            with open(os.path.join('Migrations', files)) as file:
+                if user_input in file.read():
+                    print(files)
+                    i += 1
+                    file_list_after_search.append(files)
         print('Всего найдено файлов: {}'.format(i))
         list_file = file_list_after_search
 
